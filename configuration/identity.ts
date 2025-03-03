d746e723aa0e6ffddc2bf0cad20ad3b7f02a5e47
maddychen3 <maddy.cheninfo@gmail.com>
2025-03-02 22:50:51 -0500

    Update AI Name

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Maddy`;
 export const OWNER_DESCRIPTION: string = `a beginner coder with a passion to learn`;
 
-export const AI_NAME: string = `[mini_matcha_bot]`;
+export const AI_NAME: string = `Mini Matcha Bot`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
