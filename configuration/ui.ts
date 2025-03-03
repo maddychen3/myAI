6412d768cb8d1e6b6f6e09c55f60fc003fd02b66
maddychen3 <maddy.cheninfo@gmail.com>
2025-03-03 01:28:12 -0500

    Updated MESSAGE_PLACEHOLDER and PAGE_TITLE
    export const MESSAGE_PLACEHOLDER: string = [MESSAGE PLACEHOLDER];
    
    export const CLEAR_BUTTON_TEXT: string = Erase Memory;
    export const PAGE_TITLE: string = [PAGE TITLE];

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Maddy's Mini Matcha Bot`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Ask me about any nearby coffee shops, ratings, prices, or recommendations on what to order.`;
 export const FOOTER_MESSAGE: string = `Customized AI by Maddy Chen in BUSI 488`;
 export const CLEAR_BUTTON_TEXT: string = `Erase Memory`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `Maddy's Mini Matcha Bot`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
