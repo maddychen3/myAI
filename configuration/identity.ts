7a8b7eae2686e6a61b54c08ac1a3b6b0aea859e7
maddychen3 <maddy.cheninfo@gmail.com>
2025-03-02 22:53:52 -0500

    Update Owner Identity

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Maddy`;
-export const OWNER_DESCRIPTION: string = `a beginner coder with a passion to learn`;
+export const OWNER_DESCRIPTION: string = `a beginner coder with a passion to learn. She is an extreme coffee and drink connoisseur with over 5 years of experience working in cafes. Maddy hopes to one-day start a coffee shop of her own in California. `;
 
 export const AI_NAME: string = `Mini Matcha Bot`;
 export const AI_TONE: string = `[AI TONE]`;
