85e94afbe84717130a9e5ef3e3a2af94fe90ec0c
maddychen3 <maddy.cheninfo@gmail.com>
2025-03-04 13:59:50 -0500

    Update ui.ts TITLE CHANGE AGAIN

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Maddy's Mini Matcha Bot`;
 export const MESSAGE_PLACEHOLDER: string = `Ask me about any nearby coffee shops, ratings, prices, or recommendations on what to order.`;
 export const FOOTER_MESSAGE: string = `Customized AI by Maddy Chen in BUSI 488`;
 export const CLEAR_BUTTON_TEXT: string = `Erase Memory`;
-export const PAGE_TITLE: string = `Maddy's Mini Matcha Bot`;
+export const PAGE_TITLE: string = `Mini Matcha Bot`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
